ca22b5a50e3fa30b0e096c853374d355cca6636a
wsk0715 <leindey@gmail.com>
2024-06-25 23:02:58 +0900

    fix: 예외 처리 코드 추가

--- src/main/java/com/wsk0715/central/member/controller/MemberApiController.java
+++ src/main/java/com/wsk0715/central/member/controller/MemberApiController.java
@@ -5,6 +5,7 @@ import com.wsk0715.central.member.service.MemberService;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -15,35 +16,53 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/members")
 public class MemberApiController {
 	private final MemberService memberService;
 
-	public MemberApiController(MemberService memberService) {
-		this.memberService = memberService;
-	}
-
 
 	@GetMapping
 	public Map<String, List<Member>> getAllMember() {
-		List<Member> result = memberService.selectAll();
-		Map<String, List<Member>> response = new HashMap<>(Map.of("result", result));
+		Map<String, List<Member>> response = new HashMap<>();
+
+		try {
+			List<Member> result = memberService.selectAll();
+			response.put("result", result);
+		} catch (Exception e) {
+			System.err.println("Exception during getAllMember: " + e.getClass().getName());
+			System.err.println(e.getCause().toString());
+		}
 
 		return response;
 	}
 
 	@GetMapping("/{memberId}")
 	public Map<String, Member> getMember(@PathVariable("memberId") String memberId) {
-		Member result = memberService.select(memberId);
-		Map<String, Member> response = new HashMap<>(Map.of("result", result));
+		Map<String, Member> response = new HashMap<>();
+
+		try {
+			Member result = memberService.select(memberId);
+			response.put("result", result);
+		} catch (Exception e) {
+			System.err.println("Exception during getMember: " + e.getClass().getName());
+			System.err.println(e.getCause().toString());
+		}
 
 		return response;
 	}
 
 	@PostMapping
 	public Map<String, Boolean> postMember(@RequestBody Member member) {
-		boolean result = memberService.insert(member);
-		Map<String, Boolean> response = new HashMap<>(Map.of("result", result));
+		Map<String, Boolean> response = new HashMap<>();
+
+		try {
+			boolean result = memberService.insert(member);
+			response.put("result", result);
+		} catch (Exception e) {
+			System.err.println("Exception during postMember: " + e.getClass().getName());
+			System.err.println(e.getCause().toString());
+		}
 
 		return response;
 	}
@@ -51,16 +70,30 @@ public class MemberApiController {
 	@PatchMapping("/{memberId}")
 	public Map<String, Boolean> patchMember(@PathVariable("memberId") String memberId,
 											@RequestBody Member member) {
-		boolean result = memberService.update(memberId, member);
-		Map<String, Boolean> response = new HashMap<>(Map.of("result", result));
+		Map<String, Boolean> response = new HashMap<>();
+
+		try {
+			boolean result = memberService.update(memberId, member);
+			response.put("result", result);
+		} catch (Exception e) {
+			System.err.println("Exception during patchMember: " + e.getClass().getName());
+			System.err.println(e.getCause().toString());
+		}
 
 		return response;
 	}
 
 	@DeleteMapping("/{memberId}")
 	public Map<String, Boolean> deleteMember(@PathVariable("memberId") String memberId) {
-		boolean result = memberService.delete(memberId);
-		Map<String, Boolean> response = new HashMap<>(Map.of("result", result));
+		Map<String, Boolean> response = new HashMap<>();
+
+		try {
+			boolean result = memberService.delete(memberId);
+			response.put("result", result);
+		} catch (Exception e) {
+			System.err.println("Exception during deleteMember: " + e.getClass().getName());
+			System.err.println(e.getCause().toString());
+		}
 
 		return response;
 	}
