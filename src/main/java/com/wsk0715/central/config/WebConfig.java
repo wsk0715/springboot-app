9d7245ee5bfc447f9457ab25b4cc51114fa683a1
wsk0715 <leindey@gmail.com>
2024-07-05 21:54:51 +0900

    CI/CD: https 프로토콜 설정

--- src/main/java/com/wsk0715/central/config/WebConfig.java
+++ src/main/java/com/wsk0715/central/config/WebConfig.java
@@ -10,10 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/**")
-				.allowedOrigins("http://localhost:3000")
+				.allowedOrigins("*")
 				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 				.allowedHeaders("*")
-				.allowCredentials(true);
+				.allowCredentials(false);
 	}
 
 }
